20829dea8a46b850aa3e9d4778695117047125d2
Andrey <sbs469@mail.ru>
2025-08-07 19:07:21 +0300

    Сделано базовое определение цветка

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import asyncio
+import logging
 
 import src.config.config as config
 import src.bot.mybot as bot
 
 conf = config.Config()
-tg = bot.MyBot(conf.bot_token)
+tg = bot.MyBot(conf)
 
 async def main():
     await tg.run()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
--- src/bot/mybot.py
+++ src/bot/mybot.py
@@ -1,16 +1,30 @@
+import io
+
+import imghdr
+import requests
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, BotCommand
+from aiogram.types import Message
+from aiohttp import request
+
+from src.config.config import Config
+
+import src.config.config
 
 import src.bot.keyboards as kb
 
+
 class MyBot:
-    def __init__(self, token: str):
-        self.bot = Bot(token=token)
+    def __init__(self, config: Config):
+        self.ai_token = config.ai_token
+        self.bot = Bot(token=config.bot_token)
         self.dp = Dispatcher()
         self.dp.message.register(self.start, CommandStart())
         self.dp.message.register(self.help, F.text == ('помощь'))
         self.dp.message.register(self.gitler, F.text == "pivo")
+        self.dp.message.register(self.handle_photo)
+
 
 
     async def start(self, message: Message):
@@ -61,3 +75,52 @@ ja, wir wollen's!''')
 
     async def run(self):
         await self.dp.start_polling(self.bot)
+
+    async def handle_photo(self, message: Message):
+        try:
+            print("Начата обработка изображения")
+            await message.reply("Обрабатываю изображение...")
+            photo_id = message.photo[-1].file_id
+            file = await self.bot.get_file(photo_id)
+            buffer = io.BytesIO()
+            await self.bot.download_file(file.file_path, buffer)
+            buffer.seek(0)
+            photo_bytes = buffer.read()
+            mime_type = "image/"
+            photo_type = imghdr.what(None, photo_bytes)
+            if not photo_type:
+                await message.reply(
+                    "Не удалось определить тип изображения. Возможно ваш файл поврежден. Отправьте его повторно или попробуйте другое изображение.")
+                return
+            files = {'image': (f'image_from_user.{photo_type}', io.BytesIO(photo_bytes), mime_type)}
+            headers = {'Api-Key': self.ai_token}
+            response = requests.post("https://api.plant.id/v3/identification", files=files, headers=headers)
+            response.raise_for_status()
+            print(response.json())
+            response_json = response.json()
+            # Формируем строку ответа для пользователя
+            if 'result' in response_json and 'classification' in response_json['result'] and 'suggestions' in response_json['result']['classification']:
+                suggestions = response_json['result']['classification']['suggestions']
+                if suggestions:
+                    top_suggestion = suggestions[0]
+                    plant_name = top_suggestion.get('name', 'Не определено')
+                    probability = top_suggestion.get('probability', 'Не определено')
+                    try:
+                        probability = round(float(probability * 100))
+                        if probability < 5:
+                            probability = "Не определено"
+                    except ValueError:
+                        probability = "Не определено"
+                    response_text = f"Вероятнее всего это: {plant_name} (Вероятность: {probability}%)"
+                else:
+                    response_text = "Не удалось определить растение."
+            else:
+                response_text = "Не удалось получить информацию о растении."
+
+            await message.answer(response_text)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            await message.reply(f"Ошибка запроса к нейронке:\n{e}")
+        except Exception as e:
+            print(e)
+            await message.reply(f"Неожиданная ошибка:\n{e}")
