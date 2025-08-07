47fdc3bcf82f8557dc0f50299de0e6d0bab34925
LORG <dizir10@gmail.com>
2025-08-07 15:56:55 +0300

    keyboards2

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -1,4 +1,5 @@
 from  aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="помощь")]])
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="помощь")]],
+                           input_field_placeholder='Ваш цветок')
 
