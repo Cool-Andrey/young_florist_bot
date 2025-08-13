d055c1c70d31c3837083a97af0a60a479191664a
avas1205dr <i@xmail.ru>
2025-08-13 09:47:42 +0300

    Небольшие технические изменения

--- src/bot/mybot.py
+++ src/bot/mybot.py
@@ -25,7 +25,6 @@ class MyBot:
 
         self.dp.message.register(self.menu_translate, F.text == 'язык')
         self.dp.message.register(self.help, F.text == 'помощь')
-        self.dp.message.register(self.pivo, F.text == "pivo")
         self.dp.message.register(self.handle_location, F.location)
         self.dp.message.register(self.geolocation, F.text == 'местоположение')
         self.dp.message.register(self.more_details, F.text == 'подробнее')
@@ -99,39 +98,6 @@ class MyBot:
 
 If you specify your location, the results will be more accurate!''')
 
-    async def pivo(self, message: Message):
-        await message.answer('''Was wollen wir trinken,
-sieben Tage lang?
-Was wollen wir trinken,
-ja, was denn?
-Wir wollen trinken,
-sieben Tage lang!
-Wir wollen trinken,
-ja, wir wollen's!
-Was wollen wir essen,
-sieben Tage lang?
-Was wollen wir essen,
-ja, was denn?
-Wir wollen essen,
-sieben Tage lang!
-Wir wollen essen,
-ja, wir wollen's!
-Was wollen wir tanzen,
-sieben Tage lang?
-Was wollen wir tanzen,
-ja, was denn?
-Wir wollen tanzen,
-sieben Tage lang!
-Wir wollen tanzen,
-ja, wir wollen's!
-Was wollen wir lieben,
-sieben Tage lang?
-Was wollen wir lieben,
-ja, was denn?
-Wir wollen lieben,
-sieben Tage lang!
-Wir wollen lieben,
-ja, wir wollen's!''')
     def get_translate_menu(self) -> InlineKeyboardMarkup:
         return InlineKeyboardMarkup(
             inline_keyboard=[
@@ -139,6 +105,7 @@ ja, wir wollen's!''')
                 [InlineKeyboardButton(text='English (original)', callback_data='en')]
             ]
         )
+
     async def menu_translate(self, message: Message):
         language = await self.conn.get_language(message.from_user.id)
         if language == 'ru':
@@ -151,7 +118,6 @@ ja, wir wollen's!''')
         await callback.message.delete()
         await self.conn.set_user_and_language(callback.from_user.id, 'ru')
 
-        language = 'ru'
         main_keyboard = ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="местоположение", request_location=True), KeyboardButton(text="подробнее")],
@@ -166,7 +132,6 @@ ja, wir wollen's!''')
             "Язык изменён на русский.\nТеперь отправьте фото цветка.",
             reply_markup=main_keyboard
         )
-
 
     async def translate_en(self, callback: CallbackQuery):
         await callback.answer('You have chosen the language: English')
@@ -209,8 +174,8 @@ ja, wir wollen's!''')
         try:
             user_id = message.from_user.id
             access_token = await self.conn.get_token(user_id)
+            language = await self.conn.get_language(user_id)
             if access_token:
-                language = await self.conn.get_language(user_id)
                 log, lat = await self.conn.get_geoposition(user_id)
                 if language == 'ru':
                     await message.answer("Обрабатываю запрос...")
@@ -223,21 +188,39 @@ ja, wir wollen's!''')
                 for part in parts:
                     if part.strip():
                         await message.answer(part, parse_mode="HTML")
+            else:
+                if language == 'ru':
+                    await message.answer("Отправьте изображение и нажмите на кнопку повторно")
+                else:
+                    await message.answer("Send the image and click on the button again")
         except Exception as e:
             print(e)
             await message.answer(str(e))
 
     async def similar_images(self, message: Message):
-        access_token = await self.conn.get_token(message.from_user.id)
+        user_id = message.from_user.id
+        access_token = await self.conn.get_token(user_id)
+        language = await self.conn.get_language(user_id)
         if access_token:
             try:
                 similar_images = await get_similar_images(access_token, self.plant_token)
-                await self.bot.send_media_group(
-                    chat_id=message.chat.id,
-                    media=similar_images.build()
-                )
+                if similar_images:
+                    await self.bot.send_media_group(
+                        chat_id=message.chat.id,
+                        media=similar_images.build()
+                    )
+                else:
+                    if language == 'ru':
+                        await message.answer("Отправьте изображение и нажмите на кнопку повторно")
+                    else:
+                        await message.answer("Send the image and click on the button again")
             except Exception as e:
                 await message.answer(str(e))
+        else:
+            if language == 'ru':
+                await message.answer("Отправьте изображение и нажмите на кнопку повторно")
+            else:
+                await message.answer("Send the image and click on the button again")
 
     async def health_check(self, message: Message):
         language = await self.conn.get_language(message.from_user.id)
